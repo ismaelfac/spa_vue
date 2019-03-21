f74903f2c9cc87aa19a729ad9c1916036dd56a53
webmaster <ismaelfac@gmail.com>
2019-03-20 20:36:27 -0500

    PropertiesHome

--- src/components/modules/properties/PropertiesHome.vue
+++ src/components/modules/properties/PropertiesHome.vue
@@ -1,28 +1,9 @@
 <template>
-	<div>
-		<h2>Desde Properties Home</h2>
-		<vuestic-tabs :names="[$t('Arriendos'), $t('Ventas'), $t('Marcas'), $t('Suscribete'),]">
-			<div :slot="$t('Arriendos')">
-				<property-list></property-list>
-			</div>	
-			<div :slot="$t('Ventas')">
-				Segunda Pestaña
-			</div>		
-			<div :slot="$t('Marcas')">
-				Cuarta Pestaña
-			</div>	
-			<div :slot="$t('Suscribete')">
-				Tercera Pestaña
-			</div>
-		</vuestic-tabs>	
-	</div>
 </template>
 <script>
-	import PropertyList from './PropertyList'
 	export default {
 		name: 'PropertiesHome',
 		components: {
-			PropertyList
 		}
 	}
 </script>
